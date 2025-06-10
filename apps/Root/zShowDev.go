dec92daa8eeb40e67af636eeeb470395c327b383
milansuk <milansuk86@gmail.com>
2025-06-11 00:37:08 +0200

    Added: Show chat completion in Dev mode

--- apps/Root/zShowDev.go
+++ apps/Root/zShowDev.go
@@ -28,6 +28,8 @@ func (st *ShowDev) run(caller *ToolCaller, ui *UI) error {
 		return err
 	}
 
+	codeBackCd := UI_GetPalette().GetGrey(0.05)
+
 	ui.SetColumn(0, 1, 100)
 	ui.SetRow(0, 1, 100)
 
@@ -77,6 +79,9 @@ func (st *ShowDev) run(caller *ToolCaller, ui *UI) error {
 		Prompts  []*SdkToolsPrompt
 		Err      string
 		Err_line int
+
+		Generating_name string
+		Generating_msg  string
 	}
 	var sdk_app SdkToolsPrompts
 	appJs, err := callFuncGetToolData(app.Name)
@@ -88,6 +93,7 @@ func (st *ShowDev) run(caller *ToolCaller, ui *UI) error {
 		return err
 	}
 
+	isGenerating := (sdk_app.Generating_name != "")
 	{
 		prompts_path := filepath.Join("..", app.Name, "skyalt")
 		filePromptsStr, _ := os.ReadFile(prompts_path)
@@ -121,8 +127,32 @@ func (st *ShowDev) run(caller *ToolCaller, ui *UI) error {
 			return nil
 		}
 
-		SaveBt.layout.Enable = diff
-		CancelBt.layout.Enable = diff
+		if isGenerating {
+			xx := 3
+			if !app.Dev.ShowSide {
+				HeaderDiv.SetColumn(xx, 3, 3)
+				CompBt := HeaderDiv.AddButton(xx, 0, 1, 1, "Generating")
+				CompBt.Tooltip = "Show generation"
+				CompBt.clicked = func() error {
+					app.Dev.ShowSide = true
+					return nil
+				}
+				xx++
+			}
+
+			HeaderDiv.SetColumn(xx, 3, 3)
+			StopBt := HeaderDiv.AddButton(xx, 0, 1, 1, "Stop")
+			StopBt.Cd = UI_GetPalette().E
+			StopBt.Tooltip = "Stop generating"
+			StopBt.clicked = func() error {
+				//....
+				return nil
+			}
+			xx++
+		}
+
+		SaveBt.layout.Enable = diff && !isGenerating
+		CancelBt.layout.Enable = diff && !isGenerating
 
 		//back/forward buttons ....
 	}
@@ -132,6 +162,7 @@ func (st *ShowDev) run(caller *ToolCaller, ui *UI) error {
 		ed.Align_v = 0
 		ed.Linewrapping = true
 		ed.Multiline = true
+		ed.layout.Enable = !isGenerating
 	}
 
 	//Note
@@ -192,147 +223,176 @@ func (st *ShowDev) run(caller *ToolCaller, ui *UI) error {
 
 	//Side panel
 	if app.Dev.ShowSide {
+
 		ui.SetColumn(0, 1, 100)
 		ui.SetColumnResizable(1, 5, 25, 7)
 
 		SideDiv := ui.AddLayout(1, 0, 1, 1)
-		SideDiv.SetColumn(0, 1, 100)
-		SideDiv.SetRow(1, 1, 100)
 
-		{
-			var labels []string
-			var values []string
-			for _, it := range sdk_app.Prompts {
-				errStr := ""
-				if len(it.Errors) > 0 {
-					errStr = fmt.Sprintf(" (%d errors)", len(it.Errors))
-				}
-				labels = append(labels, it.Name+".go"+errStr)
-				values = append(values, it.Name+".go")
-
-			}
-
-			HeaderDiv := SideDiv.AddLayout(0, 0, 1, 1)
-			HeaderDiv.SetColumn(0, 3, 100)
-			HeaderDiv.SetColumn(1, 1, 1)
-			HeaderDiv.SetColumnFromSub(2, 5, 100)
-			HeaderDiv.ScrollV.Hide = true
-			HeaderDiv.ScrollH.Narrow = true
-
-			HeaderDiv.AddCombo(0, 0, 1, 1, &app.Dev.SideFile, labels, values)
+		if isGenerating {
+			SideDiv.SetColumn(0, 1, 100)
+			SideDiv.SetRow(1, 1, 100)
 
 			{
-				TabsDiv := HeaderDiv.AddLayout(2, 0, 1, 1)
-				TabsDiv.SetColumn(0, 2, 3)
-				TabsDiv.SetColumn(1, 2, 3)
-				TabsDiv.SetColumn(2, 2, 3)
-				TabsDiv.Back_cd = UI_GetPalette().GetGrey(0.1)
-				//TabsDiv.Border_cd = UI_GetPalette().P
-				TabsDiv.Back_rounding = true
+				HeaderDiv := SideDiv.AddLayout(0, 0, 1, 1)
+				HeaderDiv.SetColumn(0, 3, 100)
+				HeaderDiv.ScrollV.Hide = true
+				HeaderDiv.ScrollH.Narrow = true
 
-				isStruct := app.Dev.SideFile == "Structures.go"
-				if isStruct {
-					if app.Dev.SideMode == "schema" {
+				HeaderDiv.AddText(0, 0, 1, 1, "Generating code for <i>"+sdk_app.Generating_name)
+
+				CloseBt := HeaderDiv.AddButton(1, 0, 1, 1, "X")
+				CloseBt.Background = 0.25
+				CloseBt.clicked = func() error {
+					app.Dev.ShowSide = false //hide
+					return nil
+				}
+			}
+
+			//....
+			tx := SideDiv.AddText(0, 1, 1, 1, string(sdk_app.Generating_msg))
+			//tx.Linewrapping = false
+			tx.Align_v = 0
+			tx.layout.Back_cd = codeBackCd
+
+			tx.layout.VScrollToTheBottom(true, caller)
+
+		} else {
+
+			SideDiv.SetColumn(0, 1, 100)
+			SideDiv.SetRow(1, 1, 100)
+
+			{
+				var labels []string
+				var values []string
+				for _, it := range sdk_app.Prompts {
+					errStr := ""
+					if len(it.Errors) > 0 {
+						errStr = fmt.Sprintf(" (%d errors)", len(it.Errors))
+					}
+					labels = append(labels, it.Name+".go"+errStr)
+					values = append(values, it.Name+".go")
+
+				}
+
+				HeaderDiv := SideDiv.AddLayout(0, 0, 1, 1)
+				HeaderDiv.SetColumn(0, 3, 100)
+				HeaderDiv.SetColumn(1, 1, 1)
+				HeaderDiv.SetColumnFromSub(2, 5, 100)
+				HeaderDiv.ScrollV.Hide = true
+				HeaderDiv.ScrollH.Narrow = true
+
+				HeaderDiv.AddCombo(0, 0, 1, 1, &app.Dev.SideFile, labels, values)
+
+				{
+					TabsDiv := HeaderDiv.AddLayout(2, 0, 1, 1)
+					TabsDiv.SetColumn(0, 2, 3)
+					TabsDiv.SetColumn(1, 2, 3)
+					TabsDiv.SetColumn(2, 2, 3)
+					TabsDiv.Back_cd = UI_GetPalette().GetGrey(0.1)
+					//TabsDiv.Border_cd = UI_GetPalette().P
+					TabsDiv.Back_rounding = true
+
+					isStruct := app.Dev.SideFile == "Storage.go"
+					if isStruct {
+						if app.Dev.SideMode == "schema" {
+							app.Dev.SideMode = "code"
+						}
+					}
+
+					CodeBt := TabsDiv.AddButton(0, 0, 1, 1, "Code")
+					CodeBt.Background = 0.0
+					CodeBt.clicked = func() error {
 						app.Dev.SideMode = "code"
+						return nil
+					}
+					SchemaBt := TabsDiv.AddButton(1, 0, 1, 1, "Schema")
+					SchemaBt.Background = 0.0
+					SchemaBt.layout.Enable = !isStruct
+					SchemaBt.clicked = func() error {
+						app.Dev.SideMode = "schema"
+						return nil
+					}
+
+					MsgBt := TabsDiv.AddButton(2, 0, 1, 1, "Thinking")
+					MsgBt.Background = 0.0
+					MsgBt.clicked = func() error {
+						app.Dev.SideMode = "msg"
+						return nil
+					}
+
+					switch app.Dev.SideMode {
+					case "schema":
+						SchemaBt.Background = 1
+					case "msg":
+						MsgBt.Background = 1
+					default: //"code"
+						CodeBt.Background = 1
 					}
 				}
 
-				CodeBt := TabsDiv.AddButton(0, 0, 1, 1, "Code")
-				CodeBt.Background = 0.0
-				CodeBt.clicked = func() error {
-					app.Dev.SideMode = "code"
+				CloseBt := HeaderDiv.AddButton(3, 0, 1, 1, "X")
+				CloseBt.Background = 0.25
+				CloseBt.clicked = func() error {
+					app.Dev.ShowSide = false //hide
 					return nil
-				}
-				SchemaBt := TabsDiv.AddButton(1, 0, 1, 1, "Schema")
-				SchemaBt.Background = 0.0
-				SchemaBt.layout.Enable = !isStruct
-				SchemaBt.clicked = func() error {
-					app.Dev.SideMode = "schema"
-					return nil
-				}
-
-				MsgBt := TabsDiv.AddButton(2, 0, 1, 1, "Thinking")
-				MsgBt.Background = 0.0
-				MsgBt.clicked = func() error {
-					app.Dev.SideMode = "msg"
-					return nil
-				}
-
-				switch app.Dev.SideMode {
-				case "schema":
-					SchemaBt.Background = 1
-				case "msg":
-					MsgBt.Background = 1
-				default: //"code"
-					CodeBt.Background = 1
 				}
 			}
 
-			CloseBt := HeaderDiv.AddButton(3, 0, 1, 1, "X")
-			CloseBt.Background = 0.25
-			CloseBt.clicked = func() error {
-				app.Dev.ShowSide = false //hide
-				return nil
-			}
-		}
+			if app.Dev.SideFile != "" {
 
-		if app.Dev.SideFile != "" {
-
-			var prompt *SdkToolsPrompt
-			app_toolName := strings.TrimSuffix(app.Dev.SideFile, filepath.Ext(app.Dev.SideFile))
-			for _, it := range sdk_app.Prompts {
-				if it.Name == app_toolName {
-					prompt = it
-					break
+				var prompt *SdkToolsPrompt
+				app_toolName := strings.TrimSuffix(app.Dev.SideFile, filepath.Ext(app.Dev.SideFile))
+				for _, it := range sdk_app.Prompts {
+					if it.Name == app_toolName {
+						prompt = it
+						break
+					}
 				}
-			}
 
-			if prompt != nil {
-
-				codeBackCd := UI_GetPalette().GetGrey(0.05)
-
-				switch app.Dev.SideMode {
-
-				case "schema":
-					tx := SideDiv.AddText(0, 1, 1, 1, string(prompt.Schema))
-					//tx.Linewrapping = false
-					tx.Align_v = 0
-					tx.layout.Back_cd = codeBackCd
-
-					//scroll
-					//tx.layout.VScrollToTheBottom(true, caller)
-
-				case "msg":
-					tx := SideDiv.AddText(0, 1, 1, 1, prompt.Reasoning)
-					tx.Align_v = 0
-					tx.layout.Back_cd = codeBackCd
-
-				default:
-					fl, err := os.ReadFile(filepath.Join("..", app.Name, app.Dev.SideFile))
-					if err == nil {
-						tx := SideDiv.AddText(0, 1, 1, 1, string(fl))
-						tx.Linewrapping = false
+				if prompt != nil {
+					switch app.Dev.SideMode {
+					case "schema":
+						tx := SideDiv.AddText(0, 1, 1, 1, string(prompt.Schema))
+						//tx.Linewrapping = false
 						tx.Align_v = 0
 						tx.layout.Back_cd = codeBackCd
+
+						//scroll
+						//tx.layout.VScrollToTheBottom(true, caller)
+
+					case "msg":
+						tx := SideDiv.AddText(0, 1, 1, 1, prompt.Reasoning)
+						tx.Align_v = 0
+						tx.layout.Back_cd = codeBackCd
+
+					default:
+						fl, err := os.ReadFile(filepath.Join("..", app.Name, app.Dev.SideFile))
+						if err == nil {
+							tx := SideDiv.AddText(0, 1, 1, 1, string(fl))
+							tx.Linewrapping = false
+							tx.Align_v = 0
+							tx.layout.Back_cd = codeBackCd
+						}
 					}
-				}
 
-				//Price
-				{
-					tx := SideDiv.AddText(0, 2, 1, 1, fmt.Sprintf("<i>$%.3f", prompt.Usage.Prompt_price+prompt.Usage.Input_cached_price+prompt.Usage.Completion_price+prompt.Usage.Reasoning_price))
-					tx.Align_h = 2
-				}
+					//Price
+					{
+						tx := SideDiv.AddText(0, 2, 1, 1, fmt.Sprintf("<i>$%.3f", prompt.Usage.Prompt_price+prompt.Usage.Input_cached_price+prompt.Usage.Completion_price+prompt.Usage.Reasoning_price))
+						tx.Align_h = 2
+					}
 
-				//Errors
-				{
-					if len(prompt.Errors) > 0 {
-						SideDiv.AddText(0, 3, 1, 1, "Code errors").Align_h = 1
-						SideDiv.SetRowFromSub(4, 1, 5)
-						ErrsDiv := SideDiv.AddLayout(0, 4, 1, 1)
-						ErrsDiv.SetColumn(0, 1, 100)
-						for i, er := range prompt.Errors {
-							tx := ErrsDiv.AddText(0, i, 1, 1, fmt.Sprintf("%d:%d: %s", er.Line, er.Col, er.Msg))
-							tx.Cd = UI_GetPalette().E
+					//Errors
+					{
+						if len(prompt.Errors) > 0 {
+							SideDiv.AddText(0, 3, 1, 1, "Code errors").Align_h = 1
+							SideDiv.SetRowFromSub(4, 1, 5)
+							ErrsDiv := SideDiv.AddLayout(0, 4, 1, 1)
+							ErrsDiv.SetColumn(0, 1, 100)
+							for i, er := range prompt.Errors {
+								tx := ErrsDiv.AddText(0, i, 1, 1, fmt.Sprintf("%d:%d: %s", er.Line, er.Col, er.Msg))
+								tx.Cd = UI_GetPalette().E
+							}
 						}
 					}
 				}
