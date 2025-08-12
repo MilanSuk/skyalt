adba867937163dab144aa6b24292b2d45afc920c
milansuk <milansuk86@gmail.com>
2025-08-12 18:00:20 +0200

    Fixed: Scroll inside scroll for longer LLMTips in Prompt panel

--- apps/Root/ShowPrompt.go
+++ apps/Root/ShowPrompt.go
@@ -247,7 +247,7 @@ func (st *ShowPrompt) run(caller *ToolCaller, ui *UI) error {
 				continue //skip
 			}
 
-			TipsDiv.SetRowFromSub(yy, 1, 3, true)
+			TipsDiv.SetRowFromSub(yy, 1, 100, true)
 			TipsDiv.AddText(0, yy, 1, 1, input.Picks[i].Cd.GetLabel())
 			TipsDiv.AddText(1, yy, 1, 1, strings.TrimSpace(br.LLMTip)).setMultilined()
 			yy++
